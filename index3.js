a092ffaa6184ac025f6e773e0b9290c4c81d0b0b
Start Coding Student 2 <aiden.navarro@west-mec.org>
2015-10-08 17:32:10 -0700

    Created a bar graph with LA zoo attendance. Changed the background to aqua. Added a border around graph.

--- index3.js
+++ index3.js
@@ -1,3 +1,33 @@
 /**
  * Created by session2 on 10/7/15.
  */
+google.load('visualization', '1', {packages: ['corechart', 'bar']});
+google.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Total Visitors',],
+        ['2008', 1556162],
+        ['2009', 1459080],
+        ['2010', 1543232],
+        ['2011', 1660450],
+        ['2012', 1506274]
+    ]);
+
+    var options = {
+        title: 'LA Zoo Attendance',
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Total Visitors',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Year'
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
